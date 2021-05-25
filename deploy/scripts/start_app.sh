fb4da2d1eee1c5cd8473274e2c28305bc1e0a63a
dmitriyokhrimenko <dm.okhrimenko@gmail.com>
2021-05-25 23:27:23 +0300

    Create start_app.sh

---
+++ deploy/scripts/start_app.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+source /home/ubuntu/.profile
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+cd /home/ubuntu/server
+
+ls -la
+
+whoami
+
+pwd
+
+npm install
